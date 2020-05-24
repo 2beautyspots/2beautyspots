526f793cf89fbfecd3f030c4cde14cfcbfb8258f
2beautyspots <mariverbytska@gmail.com>
2020-05-24 23:11:53 +0300

    3 pages

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-( function( $ ) {
-$( document ).ready(function() {
-
-const text = document.querySelectorAll(".text");
-const halfX = window.innerWidth / 2;
-const halfY = window.innerHeight / 2;
-
-text.forEach((el, i) => {
-  TweenMax.to(el, 1, {
-    z: 1 * (i + 8) });
-
+$(document).ready(function(){
+	$('.zoom').zoom({ on:'click' });
 });
-
-document.addEventListener("mousemove", e => {
-  text.forEach((el, i) => {
-    TweenMax.to(el, 2.0, {
-      x: (e.clientX - halfX) * (i + 1) * 0.001,
-      y: (e.clientY - halfY) * (i + 1) * 0.001 });
-
-  });
-});
-
-
-});
-} )( jQuery );
