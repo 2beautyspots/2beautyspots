2d8a27913d5341f380b2c958dace450540291430
2beautyspots <mariverbytska@gmail.com>
2020-05-30 18:50:07 +0300

    patterns - animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
 	$('.zoom').zoom({ on:'click' });
+
+    // loop();
+    $('#skate').animate({ 'background-position-y': '-100000000000000%'}, 20000000000000000, 'linear');
+    $('#girls').animate({ 'background-position-x': '100000000000000%'}, 90000000000000000, 'linear');
+    $('#corral').animate({ 'background-position-x': '-100000000000000%'}, 50000000000000000, 'linear');
 });
