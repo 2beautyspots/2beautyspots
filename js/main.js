c4939cba32fe88a6c10049dbe30c3ad590a2e7ae
2beautyspots <mariverbytska@gmail.com>
2020-05-31 17:51:01 +0300

    Menu to mobile

--- js/main.js
+++ js/main.js
@@ -4,5 +4,6 @@ $(document).ready(function(){
     // loop();
     $('#skate').animate({ 'background-position-y': '-100000000000000%'}, 20000000000000000, 'linear');
     $('#girls').animate({ 'background-position-x': '100000000000000%'}, 90000000000000000, 'linear');
-    $('#corral').animate({ 'background-position-x': '-100000000000000%'}, 50000000000000000, 'linear');
+    // $('#floral').animate({ 'background-position-x': '-100000000000000%'}, 50000000000000000, 'linear');
+    // $('#corral').animate({ 'background-position-x': '-100000000000000%'}, 50000000000000000, 'linear');
 });
